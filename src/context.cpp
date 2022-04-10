94248f66153c7ba3e60c0776a31a19564499b08d
Viktor Chegodaev <vitas1648@gmail.com>
2022-04-10 11:43:46 +0500

    Added Constructor, Destructor, Set and Get functions for scalar variables

--- src/context.cpp
+++ src/context.cpp
@@ -1,1 +1,21 @@
 #include "context.hpp"
+
+Context::Context() {
+    this->age = 0;
+    this->isMask = false;
+};
+Context::~Context() {
+
+};
+int Context::getAge() const {
+    return this->age;
+};
+void Context::setAge(int age) {
+    this->age = age;
+};
+bool Context::getIsMask() const {
+    return this->isMask;
+};
+void Context::setIsMask(bool isMask) {
+    this->isMask = isMask;
+};
--- src/context.hpp
+++ src/context.hpp
@@ -8,6 +8,11 @@ class Context
         std::vector<Context> contexts;
     
     public:
-    
+        Context();
+        ~Context();
+        int getAge() const;
+        void setAge(int age);
+        bool getIsMask() const;
+        void setIsMask(bool isMask);
 };
 
